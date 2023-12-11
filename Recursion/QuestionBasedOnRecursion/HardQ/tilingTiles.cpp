0588c698ebe41adb09bc7521c38783300822db76
Dhiraj singh <dhirajsingh1224233@gmail.com>
2023-12-11 10:35:16 +0530

    palce tiles

---
+++ Recursion/QuestionBasedOnRecursion/HardQ/tilingTiles.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int placeTiles(int n, int m){
+    // base case
+    if(n<m){
+        return 1;
+    }
+    if(n == m){
+        return 2;
+    }
+    // vertically
+    int verticallyPlace = placeTiles(n-m, m);
+    // horizontally
+    int horizontallyPalce = placeTiles(n-1, m);
+
+    return verticallyPlace+horizontallyPalce;
+}
+int main() {
+    int n = 4;
+    int m = 2;
+    cout<<"no of ways to place tiles"<<" "<<placeTiles(n,m)<<endl;
+    return 0;
+}
