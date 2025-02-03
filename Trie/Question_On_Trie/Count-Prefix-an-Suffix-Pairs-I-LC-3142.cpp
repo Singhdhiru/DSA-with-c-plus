0786905cc1a8c2c75fae660fe7a44bd154f7628e
Dhiraj singh <dhirajsingh1224233@gmail.com>
2025-02-03 23:57:49 +0530

    LC-3142 Trie based Q

---
+++ Trie/Question_On_Trie/Count-Prefix-an-Suffix-Pairs-I-LC-3142.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm> // For std::sort
+
+using namespace std;
+
+//* Approach-1 Brute Force
+// T.C : O(n^2 * l)
+// S.C : O(n^2 * l)
+class Solution
+{
+public:
+    bool isPrefixAndSuffix(string &check, string &s)
+    {
+        int n = check.length();
+        // You can use find() function as well
+        string s1 = s.substr(0, n);
+        string s2 = s.substr(s.length() - n, n);
+        return s1 == check && s2 == check;
+    }
+
+    int countPrefixSuffixPairs(vector<string> &words)
+    {
+        auto lambda = [](string &s1, string &s2)
+        {
+            return s1.length() < s2.length();
+        };
+        // sort(begin(words), end(words), lambda);
+
+        int count = 0;
+        for (int i = 0; i < words.size() - 1; i++)
+        {
+            for (int j = i + 1; j < words.size(); j++)
+            {
+                if (words[i].length() <= words[j].length() && isPrefixAndSuffix(words[i], words[j]))
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+//*Approach-2 - Using TRIE
+// T.C : ~O(n^2*l), n = number of words, l = average length of each word
+// S.C : ~O(n*l), need to store all characters of words
+struct trieNode
+{
+    trieNode *children[26];
+    bool isEndOfWord;
+};
+trieNode *getNode()
+{
+    trieNode *newNode = new trieNode();
+    for (int i = 0; i < 26; i++)
+    {
+        newNode->children[i] = NULL;
+    }
+    newNode->isEndOfWord = false;
+    return newNode;
+}
+class Trie
+{
+public:
+    trieNode *root;
+    Trie()
+    {
+        root = getNode();
+    }
+    void insert(string word)
+    {
+        trieNode *pCrawl = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            int idx = word[i] - 'a';
+            if (pCrawl->children[idx] == NULL)
+            {
+                pCrawl->children[idx] = getNode();
+            }
+            pCrawl = pCrawl->children[idx];
+        }
+        pCrawl->isEndOfWord = true;
+    }
+
+    bool searchPrefix(string prefix)
+    {
+        trieNode *pCrawl = root;
+        for (int i = 0; i < prefix.length(); i++)
+        {
+            int idx = prefix[i] - 'a';
+            if (pCrawl->children[idx] == NULL)
+            {
+                return false;
+            }
+            pCrawl = pCrawl->children[idx];
+        }
+
+        return true;
+    }
+};
+int main()
+{
+    // Sample input
+    vector<string> words = {"abc", "ab", "bc", "a", "abcabc"};
+
+    int n = words.size();
+
+    int count = 0;
+
+    for (int j = 0; j < n; j++)
+    {
+        Trie prefixTrie; // calls constrctor
+        Trie suffixTrie; // calls constructor
+
+        prefixTrie.insert(words[j]);
+        string reversed = words[j];
+        reverse(begin(reversed), end(reversed));
+
+        suffixTrie.insert(reversed);
+
+        for (int i = 0; i < j; i++)
+        {
+            if (words[i].length() > words[j].length())
+            {
+                continue;
+            }
+
+            string rev = words[i];
+            reverse(begin(rev), end(rev));
+
+            if (prefixTrie.searchPrefix(words[i]) == true && suffixTrie.searchPrefix(rev) == true)
+            {
+                count++;
+            }
+        }
+    }
+
+    cout<<"Number of perfix-suffix pairs : "<<count<<endl;
+
+    return 0;
+}
